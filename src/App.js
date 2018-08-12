18409949894e25ede36e8142cd790871cf117702
DaironSib <daironsib@gmail.com>
2018-08-12 11:45:59 +0200

    react app init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        TRELLO APP
+      </div>
+    );
+  }
+}
+
+export default App;
