68d707f09d64495d633fae5b5f7b607fd52319ad
DaironSib <daironsib@gmail.com>
2018-08-24 14:06:25 +0200

    update initialState data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import reducer from './reducers'
+import store from './store'
 import App from './App'
-import data from './store/state'
-
-const store = createStore(reducer, data)
 
 ReactDOM.render(
   <Provider store={store}>
